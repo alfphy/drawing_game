5d2d6bd3be071e474954f45042c389cde0ab04ee
alfphy <sarconaldous@gmail.com>
2026-04-09 02:24:21 +0800

    test

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+import PyQt6.QtWidgets as qtw
+import ui.containerForm
+from ui import containerForm
+
+
+class MainWindow(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = containerForm.Ui_ContainerForm()
+        self.ui.setupUi(self)
+
+app = qtw.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
+app.exec()
