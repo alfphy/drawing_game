7f16367f9a041626f77542da4f54bdbd4a04d78a
genzer <akaprincematz@gmail.com>
2026-04-10 07:03:01 +0800

    created auto convert for .ui files to .py (tried it it works, but fck some bug need to fix)

--- ui/playScene.py
+++ ui/playScene.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file 'ui/playScene.ui'
+# Form implementation generated from reading ui file 'ui\playScene.ui'
 #
 # Created by: PyQt6 UI code generator 6.11.0
 #
